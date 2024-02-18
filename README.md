d14e8eb3b411f6046fb6f58ffb0fa16d5d0a5992
arin.rao <arin.rao12@gmail.com>
2024-02-18 13:17:58 -0800

    Added pseudo code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # boing_sense
 Counting ball bounces on a table using camera and audio.
+
+
+
+## Installation
+brew install portaudio
+pip install -r requirements.txt
+
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello World')
+import cv2 
+import pyaudio
+
+def get_cam_frame(cam_handle):
+    """ Get a frame from the camera handle
+    """
+    pass
+
+
+def get_aud_frame():
+    """ Get a frame from the audio mic handle
+    """
+    pass
+
+if __name__ == "__main__":
+    # Step 1: Get a frame from the camera 
+    frame_cam = get_cam_frame(cam_handle='')
+
+    # Step 2: Get a frame from the audio mic
+    frame_aud = get_aud_frame()
+
